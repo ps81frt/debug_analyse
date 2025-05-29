2f4d534b65497a3a0954b24d943669992dd7db02
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2025-05-29 13:32:09 +0200

    Update rapport_v1.2.sh

--- rapport_v1.2.sh
+++ rapport_v1.2.sh
@@ -16,7 +16,7 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok
 echo Checking for $REQUIRED_PKG: $PKG_OK
 if [ "" = "$PKG_OK" ]; then
   echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKG
+  sudo apt install -y $REQUIRED_PKG
 fi
 
 
