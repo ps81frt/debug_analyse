84ecc1ce21919ed03a7540f89e79a42537eed644
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2025-06-19 22:17:07 +0200

    Update systemd-analyse-resume.sh

--- systemd-analyse-resume.sh
+++ systemd-analyse-resume.sh
@@ -1,5 +1,4 @@
-#!/bin/bash                                                                                                                                                                                                                                                           
-
+#!/bin/bash
 # Compatible Debian, Red Hat, Arch, etc.
 
 TOP_N=20
@@ -11,32 +10,31 @@ systemd-analyze blame --no-pager > "$tmpfile"
 {
     echo "Résumé du démarrage - Top $TOP_N services les plus lents :"
     echo "-----------------------------------------------------------"
-
     head -n $TOP_N "$tmpfile"
-
-    total_sec=0
-    while read -r line; do
-        time_part=$(echo "$line" | awk '{print $1}')
-        if [[ $time_part == *ms ]]; then
-            ms=${time_part%ms}
-            sec=$(echo "scale=3; $ms/1000" | bc)
-        elif [[ $time_part == *s ]]; then
-            sec=${time_part%s}
-        else
-            sec=0
-        fi
-        total_sec=$(echo "$total_sec + $sec" | bc)
-    done < <(head -n $TOP_N "$tmpfile")
-
+    
+    total_sec=$(head -n $TOP_N "$tmpfile" | awk '
+    {
+        time_str = $1
+        if (time_str ~ /ms$/) {
+            gsub(/ms$/, "", time_str)
+            total += time_str / 1000
+        } else if (time_str ~ /s$/) {
+            gsub(/s$/, "", time_str)
+            total += time_str
+        } else if (time_str ~ /min$/) {
+            gsub(/min$/, "", time_str)
+            total += time_str * 60
+        }
+    }
+    END { printf "%.3f", total }
+    ')
+    
     echo "-----------------------------------------------------------"
     echo "Temps total cumulé pour les top $TOP_N services : ${total_sec}s"
-
     total_services=$(wc -l < "$tmpfile")
     echo "Nombre total de services listés : $total_services"
-
 } | tee "$logfile"
 
 rm "$tmpfile"
-
 echo
 echo "Résumé enregistré dans le fichier : $logfile"
